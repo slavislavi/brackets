c4ea74696471aeefb3c74399fa88a0f4db76b052
slavislavi <slavislavi1986@gmail.com>
2020-10-11 02:01:25 +0300

    feat: brackets is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    
+    let configArray = bracketsConfig.map((i) => i[0] + i[1]);
+
+    let initStr = str;
+    while (true) {
+        for (let i = 0; i < configArray.length; i++) {
+            str = str.replace(configArray[i], '');
+        };
+
+        if (str.length == 0) {
+            return true;
+        };
+        if (initStr === str) {
+            return false;
+        };
+
+        initStr = str;
+    };
+};
